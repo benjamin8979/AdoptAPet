29cdefdd0d6cb52c9d4d6f2dfa981308a2cd9d3b
Ben Zuniga <benzun3@gmail.com>
2024-04-07 16:58:18 -0700

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
 // Add react-router-dom imports
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
 const appRouter = "REPLACE ME";
@@ -12,7 +13,7 @@ const appRouter = "REPLACE ME";
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter}/>
   );
 }
 
