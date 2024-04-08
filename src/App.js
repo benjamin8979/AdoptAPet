8b5e9f2e646c6ca61c1516222a1cee8132f16d96
Ben Zuniga <benzun3@gmail.com>
2024-04-07 17:33:55 -0700

    Create root router

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Root from './components/root';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Root/>}/>
+));
 
 function App() {
   return (
-    // replace below with a Router Provider
+    // Router Provider
     <RouterProvider router={appRouter}/>
   );
 }
