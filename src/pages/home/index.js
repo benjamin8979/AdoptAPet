797f20c81d54bd8ac6e003ade872c3564fcca3c5
Ben Zuniga <benzun3@gmail.com>
2024-04-08 17:20:04 -0700

    Add link navigation components

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,14 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { getPets } from '../../api/petfinder';
 import Hero from '../../components/hero';
 
-// import useParams
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
-// import Link
 
 const HomePage = () => {
   const [data, setData] = useState(null);
-  const { type } = useParams(); // Fix me!
+  const { type } = useParams();
 
   useEffect(() => {
     async function getPetsData() {
@@ -35,9 +33,9 @@ const HomePage = () => {
       {data.length ? (
         <div className="grid">
           {data.map((animal) => (
-            <a // Change me to a Link!
+            <Link
               key={animal.id}
-              href={`/${animal.type.toLowerCase()}/${animal.id}`}
+              to={`/${animal.type.toLowerCase()}/${animal.id}`}
               className="pet"
             >
               <article>
@@ -58,7 +56,7 @@ const HomePage = () => {
                 <p>Color: {animal.colors.primary}</p>
                 <p>Gender: {animal.gender}</p>
               </article>
-            </a> // Don't forget to change me!
+            </Link>
           ))}
         </div>
       ) : (
